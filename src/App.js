43726e6fb94ac9ad006534ea4cc07e10b0b6c59d
Snehov <snehov@gmail.com>
2019-02-28 12:18:05 +0100

    project init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CalendarInfinite from "./Calendar/CalendarInfinite";
+import Calendar from "./Calendar/Calendar";
+import logo from './logo.svg';
+//import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <CalendarInfinite /> */}
+          <hr />
+          <Calendar/>         
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
