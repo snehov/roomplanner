80d2d90b281fe64110db719d23964b1d3053df1c
Snehov <snehov@gmail.com>
2019-07-14 09:53:35 +0200

    rename calendar files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-//import CalendarInfinite from "./Calendar/CalendarInfinite";
-//import Calendar from "./Calendar/Calendar";
-import CalendarIncl from "./Calendar/CalendarIncl";
+//b import CalendarInfinite from "./Calendar/CalendarInfinite";
+//import Calendar_from_node from "./Calendar/Calendar_from_node_modules";
+import Calendar from "./Calendar/Calendar";
 //import './App.css';
 
 class App extends Component {
@@ -9,10 +9,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          {/* <CalendarInfinite /> */}
+           {/* <CalendarInfinite />  */}
           <hr />
-          {/* <Calendar/>     */}  
-            <CalendarIncl/>  
+          {/* <Calendar_from_node/>     */}  
+           <Calendar/>  
         </header>
       </div>
     );
